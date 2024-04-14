26f106490a94c0d27a8f3a79e11d43003cb22a4c
Dips2020 <dipsbhattarai2020@gmail.com>
2024-04-14 10:29:00 +0545

    dashboard file created

---
+++ react-app/src/components/HomePage/pages/Dashboard.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+const Dashboard = () => {
+  return <div className="h-[200px] w-full bg-red-200 mt-[92px]">Dashboard</div>;
+};
+
+export default Dashboard;
