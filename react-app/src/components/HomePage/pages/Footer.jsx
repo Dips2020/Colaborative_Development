3e164a27a4020ce4660d871087cfb322f8722bc7
nirmal-rawal <nirmalrawal3289@gmail.com>
2024-04-18 10:06:41 +0545

    Updated footer page

--- react-app/src/components/HomePage/pages/Footer.jsx
+++ react-app/src/components/HomePage/pages/Footer.jsx
@@ -1,25 +1,33 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
+import React from 'react';
+
+
 const Footer = () => {
   return (
-    <div className="contact short">
-    
-      <div>
-
-        <div>
-          <h3>ready to get start?</h3>
-          <h3>Talk to us today</h3>
+    <footer className="footer">
+      <div className="footer__content">
+        <div className="footer__section">
+          <h4>About Us</h4>
+          <p>We are your local farmer market providing fresh, organic produce and goods.</p>
         </div>
-
-        <div>
-          <navlink>
-            <button>Get Start</button>
-          </navlink>
+        <div className="footer__section">
+          <h4>Contact Us</h4>
+          <p>Email: info@localfarmermarket.com</p>
+          <p>Phone: +1 (123) 456-7890</p>
         </div>
-
-        
+        <div className="footer__section">
+          <h4>Follow Us</h4>
+          <p>Stay connected with us on social media for updates!</p>
+          <div className="footer__social-icons">
+            <a href="#"><i className="fab fa-facebook-f"></i></a>
+            <a href="#"><i className="fab fa-twitter"></i></a>
+            <a href="#"><i className="fab fa-instagram"></i></a>
+          </div>
+        </div>
       </div>
-    </div>
+      <div className="footer__bottom">
+        <p>&copy; 2024 Local Farmer Market. All rights reserved.</p>
+      </div>
+    </footer>
   );
 };
 
