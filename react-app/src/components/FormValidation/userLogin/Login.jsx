7c379d8689b54e6c4a206114dbcca8136c90248a
Dips2020 <dipsbhattarai2020@gmail.com>
2024-04-19 20:06:02 +0545

    changed img theme to video in both login and sign up apge

--- react-app/src/components/FormValidation/userLogin/Login.jsx
+++ react-app/src/components/FormValidation/userLogin/Login.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { Formik } from "formik";
 import TextField from "../userSignUp/TextField";
-import firstImg from "../../../assets/firstImg.png";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../GoogleAuth/UserGoogleAuthentication";
 import { FcGoogle } from "react-icons/fc";
@@ -71,64 +70,68 @@ const Login = () => {
         <div className="bg-black bg-opacity-[70%] h-screen w-screen flex justify-center items-center absolute z-50">
           <div className="flex w-[1200px] h-[550px] justify-center items-center shadow-md">
             <div className="h-[550px] w-[50%]">
-              <img
-                src={firstImg}
-                alt="First img"
-                className="object-cover w-full h-full rounded-l-lg"
-              />
+              <video
+                // src="https://videos.pexels.com/video-files/7031954/7031954-uhd_3840_2160_24fps.mp4"
+                src="https://videos.pexels.com/video-files/2806063/2806063-hd_1080_1920_30fps.mp4"
+                className="object-cover w-full h-full rounded-r"
+                autoPlay
+                muted
+                loop
+              ></video>
             </div>
 
             <form
               onSubmit={handleSubmit}
-              className="bg-white h-[550px] w-[50%] flex flex-col justify-center items-center space-y-2 rounded-r-lg relative"
+              className="bg-white h-[550px] w-[50%] flex flex-col justify-center items-center rounded-r-lg relative"
             >
-              <div className="h-[10%] w-full text-center">
+              <div className="h-[50px] w-full text-center">
                 <h1 className="font-bold text-2xl text-black">
                   Welcome to Login Page !!
                 </h1>
               </div>
               <div className="h-[85%] w-full flex flex-col justify-center items-center space-y-6">
-                <div className="h-[30%] w-full flex justify-center items-center relative">
-                  <div>
-                    <TextField name="email" label="Email" />
-                    {invalidEmailMsg ? (
-                      <div className="absolute bottom-5 left-[40px] font-semibold text-[16px] text-red-500">
-                        Invalid Email !
+                <div className="h-[300px] w-[95%] flex flex-col justify-center items-center relative border-b-2 border-gray-300">
+                  <div className="h-[50%] w-full flex justify-center items-center relative">
+                    <div>
+                      <TextField name="email" label="Email" />
+                      {invalidEmailMsg ? (
+                        <div className="absolute bottom-5 left-[40px] font-semibold text-[16px] text-red-500">
+                          Invalid Email !
+                        </div>
+                      ) : null}
+                    </div>
+                    <div>
+                      <TextField
+                        name="password"
+                        label="Password"
+                        type="password"
+                      />
+                    </div>
+                    {invalidPasswordMsg ? (
+                      <div className="absolute bottom-5 right-[40px] font-semibold text-[16px] text-red-500">
+                        Invalid Password !
                       </div>
                     ) : null}
                   </div>
-                  <div>
-                    <TextField
-                      name="password"
-                      label="Password"
-                      type="password"
-                    />
+                  <div className="h-[30%] w-full flex justify-center items-center space-x-6">
+                    <button
+                      type="submit"
+                      className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300 w-[220px] h-[50px]"
+                    >
+                      Login
+                    </button>
+                    <h1 className="font-bold text-[18px]">OR</h1>
+                    <button
+                      type="button"
+                      onClick={handleGoogleSignIn}
+                      className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-[10px] rounded-lg transition duration-300 w-[220px] h-[50px] flex justify-evenly items-center"
+                    >
+                      <span className="text-2xl bg-white rounded-sm">
+                        <FcGoogle />
+                      </span>
+                      Continue with Google
+                    </button>
                   </div>
-                  {invalidPasswordMsg ? (
-                    <div className="absolute bottom-5 right-[40px] font-semibold text-[16px] text-red-500">
-                      Invalid Password !
-                    </div>
-                  ) : null}
-                </div>
-                <div className="h-[20%] w-full flex justify-center items-center space-x-6">
-                  <button
-                    type="submit"
-                    className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300 w-[220px] h-[50px]"
-                  >
-                    Login
-                  </button>
-                  <br />
-                  <h1 className="font-bold text-[18px]">OR</h1>
-                  <button
-                    type="button"
-                    onClick={handleGoogleSignIn}
-                    className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-[10px] rounded-lg transition duration-300 w-[220px] h-[50px] flex justify-evenly items-center"
-                  >
-                    <span className="text-2xl bg-white rounded-sm">
-                      <FcGoogle />
-                    </span>
-                    Continue with Google
-                  </button>
                 </div>
                 <div className="h-[20%] w-full flex justify-center items-center space-x-2">
                   <h2> Account not registered? </h2>
