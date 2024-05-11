d0752fa8b9d1f3e7d6319e499d2ef1e64f83f5fc
Tenjing Sherpa <10jingsherpa@gmail.com>
2024-05-12 00:11:14 +0545

    Added Tailwind Css for style of add to cart site

--- react-app/src/components/HomePage/pages/AddToCart/AddToCart.jsx
+++ react-app/src/components/HomePage/pages/AddToCart/AddToCart.jsx
@@ -1,79 +1,134 @@
-/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
 import React from "react";
-import { ImCross } from "react-icons/im";
+import { AiOutlinePlus } from "react-icons/ai";
+import { FaMinus } from "react-icons/fa6";
+import { RxCross2 } from "react-icons/rx";
 
 const AddToCart = () => {
   return (
-    <div className="shopping-cart-container">
-      <div className="inner-content">
-        <div className="cart-content">
-          <div className="left-section">
-            <h2 className="cart-title">Shopping Cart</h2>
-            <div className="product-container">
-              {/* Product 1 */}
-              <div className="product-card">
-                <img
-                  className="product-image"
-                  src="https://firebasestorage.googleapis.com/v0/b/test-2-69499.appspot.com/o/ProductsImg%2F0661ab15-eff0-443b-87b4-da254467c5e7?alt=media&token=fd906434-e0a1-4eb5-b19b-cbe166dab477"
-                  alt="Shirt 1"
-                />
-                <div className="product-details">
-                  <h3 className="product-title">Peanuts</h3>
+    <div className="bg-gray-100  w-full h-full mt-[100px] flex justify-center items-center font-sans">
+      <div className="container mx-auto px-4 py-8">
+        <div className="bg-white rounded-lg shadow-md flex">
+          {/* Cart */}
+          <div className="w-[75%] p-8">
+            <h2 className="text-xl font-bold mb-4">Shopping Cart</h2>
+            {/* Cart Items */}
+            <div className="border-b border-gray-200 mb-4">
+              {/* Item 1 */}
+              <div className="flex items-center justify-between py-4">
+                <div className="flex items-center">
+                  <img
+                    className="w-[300px] h-[200px] rounded-lg mr-4"
+                    src="https://firebasestorage.googleapis.com/v0/b/test-2-69499.appspot.com/o/ProductsImg%2F7a44a66b-2ec6-4ca6-b613-244e740832c0?alt=media&token=84b60871-37b6-45d0-8a1b-27c6d95901aa"
+                    alt="Shirt"
+                  />
                   <div>
-                    <p className="product-info">Price: $35</p>
-                    <p className="product-info">Dry Fruits</p>
+                    <h3 className="text-lg font-semibold">Buffalo Meat</h3>
+
+                    <p className="text-sm text-gray-600">Fresh Meats</p>
                   </div>
                 </div>
-                <ImCross className="remove-icon" />
-              </div>
-              {/* Product 2 */}
-              <div className="product-card">
-                <img
-                  className="product-image"
-                  src="https://firebasestorage.googleapis.com/v0/b/test-2-69499.appspot.com/o/ProductsImg%2F0661ab15-eff0-443b-87b4-da254467c5e7?alt=media&token=fd906434-e0a1-4eb5-b19b-cbe166dab477"
-                  alt="Shirt 1"
-                />
-                <div className="product-details">
-                  <h3 className="product-title">Peanuts</h3>
-                  <div>
-                    <p className="product-info">Price: $35</p>
-                    <p className="product-info">Dry Fruits</p>
-                  </div>
+                <div className="flex items-center">
+                  <button className="border px-3 py-1 mr-2">
+                    <FaMinus />
+                  </button>
+                  <span className="border px-3 py-1">1</span>
+                  <button className="border px-3 py-1 ml-2">
+                    <AiOutlinePlus />
+                  </button>
+                  <span className="text-sm ml-4">$4.00</span>
+                  <button className="ml-2">
+                    <RxCross2 />
+                  </button>
                 </div>
-                <ImCross className="remove-icon" />
-              </div>
-              {/* Product 3 */}
-              <div className="product-card">
-                <img
-                  className="product-image"
-                  src="https://firebasestorage.googleapis.com/v0/b/test-2-69499.appspot.com/o/ProductsImg%2F0661ab15-eff0-443b-87b4-da254467c5e7?alt=media&token=fd906434-e0a1-4eb5-b19b-cbe166dab477"
-                  alt="Shirt 1"
-                />
-                <div className="product-details">
-                  <h3 className="product-title">Peanuts</h3>
-                  <div>
-                    <p className="product-info">Price: $35</p>
-                    <p className="product-info">Dry Fruits</p>
-                  </div>
-                </div>
-                <ImCross className="remove-icon" />
               </div>
             </div>
+
+            {/* Cart Items */}
+            <div className="border-b border-gray-200 mb-4">
+              {/* Item 1 */}
+              <div className="flex items-center justify-between py-4">
+                <div className="flex items-center">
+                  <img
+                    className="w-[300px] h-[200px] rounded-lg mr-4"
+                    src="https://firebasestorage.googleapis.com/v0/b/test-2-69499.appspot.com/o/ProductsImg%2F7a44a66b-2ec6-4ca6-b613-244e740832c0?alt=media&token=84b60871-37b6-45d0-8a1b-27c6d95901aa"
+                    alt="Shirt"
+                  />
+                  <div>
+                    <h3 className="text-lg font-semibold">Buffalo Meat</h3>
+
+                    <p className="text-sm text-gray-600">Fresh Meats</p>
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <button className="border px-3 py-1 mr-2">
+                    <FaMinus />
+                  </button>
+                  <span className="border px-3 py-1">1</span>
+                  <button className="border px-3 py-1 ml-2">
+                    <AiOutlinePlus />
+                  </button>
+                  <span className="text-sm ml-4">$4.00</span>
+                  <button className="ml-2">
+                    <RxCross2 />
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* Cart Items */}
+            <div className="border-b border-gray-200 mb-4">
+              {/* Item 1 */}
+              <div className="flex items-center justify-between py-4">
+                <div className="flex items-center">
+                  <img
+                    className="w-[300px] h-[200px] rounded-lg mr-4"
+                    src="https://firebasestorage.googleapis.com/v0/b/test-2-69499.appspot.com/o/ProductsImg%2F7a44a66b-2ec6-4ca6-b613-244e740832c0?alt=media&token=84b60871-37b6-45d0-8a1b-27c6d95901aa"
+                    alt="Shirt"
+                  />
+                  <div>
+                    <h3 className="text-lg font-semibold">Buffalo Meat</h3>
+
+                    <p className="text-sm text-gray-600">Fresh Meats</p>
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <button className="border px-3 py-1 mr-2">
+                    <FaMinus />
+                  </button>
+                  <span className="border px-3 py-1">1</span>
+                  <button className="border px-3 py-1 ml-2">
+                    <AiOutlinePlus />
+                  </button>
+                  <span className="text-sm ml-4">$4.00</span>
+                  <button className="ml-2">
+                    <RxCross2 />
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* Add more items here */}
           </div>
           {/* Summary */}
-          <div className="right-section">
-            <h3 className="summary-title">Summary</h3>
-            <hr className="divider" />
-            <div className="summary-info">
-              <span>ITEMS 3</span>
-              <span>&euro; 132.00</span>
+          <div className="w-[25%] bg-gray-50 p-8 rounded-r-lg">
+            <h2 className="text-xl font-bold mb-4">Summary</h2>
+            <div className="mb-4">
+              <p className="text-sm">ITEMS 3</p>
+              <p className="text-sm text-right">$4.00</p>
             </div>
-            <div className="summary-info">
-              <span>TOTAL PRICE</span>
-              <span>&euro; 137.00</span>
+
+            <div className="border-t border-gray-200 mb-4">
+              <p className="text-sm">TOTAL PRICE</p>
+              <p className="text-sm text-right">$4.00</p>
             </div>
-            <button className="checkout-btn">CHECKOUT</button>
+            <button className="btn bg-black text-white w-full py-2">
+              CHECKOUT
+            </button>
           </div>
+        </div>
+        <div className="text-center mt-8">
+          <a href="#" className="text-gray-500 hover:underline">
+            &larr; Back to shop
+          </a>
         </div>
       </div>
     </div>
